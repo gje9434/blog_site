a8a22110dae2c2c59ece7221bb845acdb7f4a0a9
gje9434 <optimax0@hotmail.com>
2020-06-19 00:01:30 +0100

    add get requests for about and contact. handle post request for adding new blog post.

--- app.js
+++ app.js
@@ -3,18 +3,73 @@ const app = express();
 const db = require("./db");
 
 app.use(express.static("public"));
+app.use(express.urlencoded());
 app.set("view engine", "ejs");
 
 app.listen(3000, () => {
     console.log("Server started");
 })
 
+/* if post is longer than 80 chars, 
+display only first 80 chars + "..." 
+and provide link to view entire post
+in a new page. Latest post is always
+viewable in full on homepage, hence 
+starting the for-loop at 1
+*/
+shortenPosts = function(blogPosts) {
+    for(let i=1;i<blogPosts.length;i++) {
+        if(blogPosts[i].post_content.length > 80) {
+            let shortenedPost = blogPosts[i].post_content.slice(0, 50)+"...";
+            blogPosts[i].post_content = shortenedPost;
+        }
+    }
+    return blogPosts
+}
+
 app.get("/", (req, res) => {
-    db.client.query("SELECT * FROM blog_posts", (error, response) => {
+    db.client.query("SELECT * FROM blog_posts",(error, response) => {
         if(error) {
-            console.log(error.stack)
+            console.log(error.stack);
         } else {
-            res.render("index.ejs", { blogPosts: response.rows });
+            // sort posts by date order DESCENDING
+            let blogPosts = response.rows.sort((a, b) => {
+                return b.created - a.created;
+            })
+            // change TIMESTAMP to more readable date string
+            blogPosts.map(post => {
+                post.created = post.created.toDateString();
+                return post;
+            })
+            res.render("index.ejs", { blogPosts: shortenPosts(blogPosts) });
+        }
+    })   
+})
+
+app.get("/about", (req, res) => {
+    res.render("about_us.ejs");
+})
+
+app.get("/contact", (req, res) => {
+    res.render("contact_us.ejs")
+})
+
+app.get("/compose", (req, res) => {
+    res.render("compose.ejs");
+})
+
+// add a new blog post
+app.post("/compose", (req, res) => {
+    let newPostTitle = req.body.title;
+    let newPostContent = req.body.content;
+    let text = "INSERT INTO blog_posts (post_title, post_content) VALUES ($1, $2)";
+    let values = [newPostTitle, newPostContent];
+    
+    db.client.query(text, values, (error, response) => {
+        if(error) {
+            console.log(error.stack);
+        } else {
+            res.redirect("/");
         }
     })   
 })
