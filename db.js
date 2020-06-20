a2d57a766bd41b76a83695f0902fd00d46716571
gje9434 <optimax0@hotmail.com>
2020-06-20 16:58:18 +0100

    add SSL encryption to fix database connection error

--- db.js
+++ db.js
@@ -2,10 +2,11 @@ const {Client} = require("pg");
 
 const client = new Client({
     user: "svyjcxdfkxgfgp",
-    password: "5b57a71b369537535cf34e8b0ebdfc51d70f4fff64fe4a6629c9c957a0e55f81!",
+    password: "5b57a71b369537535cf34e8b0ebdfc51d70f4fff64fe4a6629c9c957a0e55f81",
     host: "ec2-79-125-26-232.eu-west-1.compute.amazonaws.com",
     port: 5432,
-    database: "db2fa0i5ab3r71"
+    database: "db2fa0i5ab3r71",
+    ssl: { rejectUnauthorized: false }
 });
 
 client.connect()
