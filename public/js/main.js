98d9b5bb4c1204673c9ac08c9234caa1191ea913
gje9434 <optimax0@hotmail.com>
2020-06-20 01:08:19 +0100

    fix snapping issue with sticky nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,46 @@
 window.onload = function() {
 
-    const submitForm = document.querySelector("form");
-    
-    const noTitleError = document.querySelector(".noTitleError");
-    const noContentError = document.querySelector(".noContentError");
+    setStickyNav = () => {
 
-    submitForm.addEventListener("submit", e => {
+        var navbar = document.querySelector(".navbar");
+        var fixed_position = navbar.offsetTop;
+
+        window.addEventListener("scroll", e => {
+            if(window.scrollY > fixed_position) { 
+                navbar.classList.add("sticky");
+                document.querySelector(".container").style = `margin-top: ${navbar.offsetHeight}px`;
+            } else {
+                navbar.classList.remove("sticky");
+                document.querySelector(".container").style = "margin-top: 0px";
+            }
+        })
+    };
+
+    listenForComposeErrors = () => {
+        const submitForm = document.querySelector("form");
         
-        noContentError.style.display = "none";
-        noTitleError.style.display = "none";
+        const noTitleError = document.querySelector(".noTitleError");
+        const noContentError = document.querySelector(".noContentError");
 
-        if(submitForm["title"].value.replace(" ", "") == "") {
-            e.preventDefault();
-            noTitleError.style.display = "block";
-        }
-        if(submitForm["content"].value.replace(" ", "") == "") {
-            e.preventDefault();
-            noContentError.style.display = "block";
-        } 
-    })
+        submitForm.addEventListener("submit", e => {
+            
+            noContentError.style.display = "none";
+            noTitleError.style.display = "none";
+
+            if(submitForm["title"].value.replace(" ", "") == "") {
+                e.preventDefault();
+                noTitleError.style.display = "block";
+            }
+            if(submitForm["content"].value.replace(" ", "") == "") {
+                e.preventDefault();
+                noContentError.style.display = "block";
+            } 
+        })
+    }
+
+    setStickyNav()
+
+    if(window.location.href.indexOf("compose") > -1) {
+        listenForComposeErrors();
+    }
 }
