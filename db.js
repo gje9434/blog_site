0e971e0fed4486d66676376ca059fb05cbf34d3d
gje9434 <optimax0@hotmail.com>
2020-06-20 16:45:29 +0100

    change database name for Heroku

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const client = new Client({
     password: "5b57a71b369537535cf34e8b0ebdfc51d70f4fff64fe4a6629c9c957a0e55f81!",
     host: "ec2-79-125-26-232.eu-west-1.compute.amazonaws.com",
     port: 5432,
-    database: "postgres"
+    database: "db2fa0i5ab3r71"
 });
 
 client.connect()
