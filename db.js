c1bb8fd98749188da51fb2cbbb324f1a68706a56
gje9434 <optimax0@hotmail.com>
2020-06-20 16:30:18 +0100

    change DB connection to connect to HEROKU db

--- db.js
+++ db.js
@@ -1,9 +1,9 @@
 const {Client} = require("pg");
 
 const client = new Client({
-    user: "postgres",
-    password: "Rainforest00!",
-    host: "127.0.0.1",
+    user: "svyjcxdfkxgfgp",
+    password: "5b57a71b369537535cf34e8b0ebdfc51d70f4fff64fe4a6629c9c957a0e55f81!",
+    host: "ec2-79-125-26-232.eu-west-1.compute.amazonaws.com",
     port: 5432,
     database: "postgres"
 });
